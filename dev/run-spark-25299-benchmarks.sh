9546397040ef77fa35579b34ae95108c8092e04b
Yifei Huang <yifeih@palantir.com>
2019-02-26 16:29:26 -0800

    blah (#495)

--- dev/run-spark-25299-benchmarks.sh
+++ dev/run-spark-25299-benchmarks.sh
@@ -15,3 +15,19 @@ mkdir -p /tmp/artifacts
 cp $SPARK_DIR/sql/core/benchmarks/BypassMergeSortShuffleWriterBenchmark-results.txt /tmp/artifacts/
 cp $SPARK_DIR/sql/core/benchmarks/UnsafeShuffleWriterBenchmark-results.txt /tmp/artifacts/
 
+
+RESULTS=""
+for benchmark_file in /tmp/artifacts/*.txt; do
+    RESULTS+=$(cat $benchmark_file)
+done
+
+PULL_REQUEST_NUM=$(git log -1 | sed "5q;d" | awk '{print $NF}' | sed 's/(//g' | sed 's/)//g' | sed 's/#//g')
+
+
+USERNAME=svc-spark-25299
+PASSWORD=$SVC_SPARK_25299_PASSWORD
+message='{"body": "```'
+message+=$results
+message+='```", "event":"COMMENT"}'
+echo $message
+curl -XPOST https://${USERNAME}:${PASSWORD}@api.github.com/repos/palantir/spark/pulls/${PULL_REQUEST_NUM}/reviews -d \'$message\'
